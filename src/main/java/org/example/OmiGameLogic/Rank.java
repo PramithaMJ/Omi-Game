42a2c60840d2d41b5a5048080fd61747b0d3c4a6
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-05-10 08:34:51 +0530

    initialize chat communication

---
+++ src/main/java/org/example/OmiGameLogic/Rank.java
@@ -0,0 +1,12 @@
+package org.example.OmiGameLogic;
+
+public enum Rank {
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+}
---
+++ src/main/java/org/example/OmiGameLogic/Suit.java
@@ -0,0 +1,8 @@
+package org.example.OmiGameLogic;
+
+public enum Suit {
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES
+}
