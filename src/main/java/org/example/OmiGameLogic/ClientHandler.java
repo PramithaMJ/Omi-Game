03064eb03553600876597e1e2b2b9e89a6c9c7ba
AGS Lakpahana <agslakpahana@gmail.com>
2024-05-11 12:48:19 +0530

    sadasd

---
+++ src/main/java/org/example/OmiGameLogic/ClientHandler.java
@@ -0,0 +1,97 @@
+package org.example.OmiGameLogic;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>(); //to keep track on all connected clients
+
+    protected Socket socket;
+    protected BufferedReader bufferedReader;
+    protected BufferedWriter bufferedWriter;
+    private String clientUsername;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            broadcastMessage("SERVER: " + clientUsername + " has entered the game!");
+            clientHandlers.add(this);
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    //everything below run method will run on multiple separate threads
+    @Override
+    public void run() {
+        String messageFromClient;
+
+        try {
+            while ((messageFromClient = bufferedReader.readLine()) != null) {
+                broadcastMessage(clientUsername + ": " + messageFromClient);
+            }
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        } finally {
+            removeClientHandler();
+        }
+    }
+
+    public void broadcastMessage2(String messageToSend) {
+        for (ClientHandler clientHandler : clientHandlers) {
+            try {
+                if (clientHandler.clientUsername.equals(clientUsername)) {
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void broadcastMessage(String messageToSend) {
+        for (ClientHandler clientHandler : clientHandlers) {
+            try {
+                if (!clientHandler.clientUsername.equals(clientUsername)) {
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void removeClientHandler() {
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: " + clientUsername + " has left the Game!");
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/client/Client1.java
@@ -0,0 +1,117 @@
+package org.example.client;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client1 {
+    public Socket socket;
+    public BufferedReader bufferedReader;
+    public BufferedWriter bufferedWriter;
+    private String username;
+
+    public Client1(Socket socket, String username){
+        try{
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.username = username;
+        }catch(IOException e){
+            closeEverything(socket,bufferedWriter,bufferedReader);
+        }
+    }
+
+    public void sendMessage(){
+        try{
+            bufferedWriter.write(username);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            Scanner scanner = new Scanner(System.in);
+
+           while(socket.isConnected()){
+                String messageToSend = scanner.nextLine();
+                bufferedWriter.write(username+": " + messageToSend);
+
+               System.out.println("Message sent: " + messageToSend);
+
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+
+        }catch(IOException e){
+            closeEverything(socket,bufferedWriter,bufferedReader);
+        }
+    }
+
+    public void listenForMessage(){
+         new Thread(new Runnable() {
+             @Override
+             public void run() {
+                 String msgFromGroupChat;
+
+                 while(socket.isConnected()){
+                     try{
+                         msgFromGroupChat = bufferedReader.readLine();
+                         System.out.println(msgFromGroupChat);
+
+                     }catch(IOException e){
+                         closeEverything(socket,bufferedWriter,bufferedReader);
+                     }
+                 }
+             }
+         }).start();
+    }
+
+
+
+
+
+    public void closeEverything(Socket socket,BufferedWriter bufferedWriter,BufferedReader bufferedReader){
+        try{
+            if(bufferedReader!=null){
+                bufferedReader.close();
+            }
+            if(bufferedWriter!=null){
+                bufferedWriter.close();
+            }
+            if(socket!=null){
+                socket.close();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessageFromServer(String message) {
+        try {
+            bufferedWriter.write(message);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your username for the omi game: ");
+        String username = scanner.nextLine();
+        Socket socket = new Socket("localhost",1234);
+        Client1 client1 = new Client1(socket,username);
+        client1.listenForMessage();
+        client1.sendMessage();
+
+    }
+
+    public void executePlay10Rounds() {
+        try {
+            bufferedWriter.write("playRound10");
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
